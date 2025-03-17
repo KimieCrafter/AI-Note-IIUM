fd70b89d0efd42638fdf4243860e049d915144b3
[KimieCrafter] <[kimiecrafter2003@gmail.com]>
2025-03-17 20:37:22 +0800

    Changes

--- Machine_Learning/Asgmt/Convert.py
+++ Machine_Learning/Asgmt/Convert.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load the Excel file
-excel_data = pd.read_excel('SolarPowerGenerationKaggle_MissingData.xlsx')
+excel_data = pd.read_excel('Solar_Power_Generation.xlsx')
 
 # Save as CSV
 excel_data.to_csv('converted_data.csv', index=False)
